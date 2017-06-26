76579acc5cd42d8b230bd62f42717636da81dfca
escakot <ec@neorennie.com>
2017-06-26 17:20:05 -0400

    Initial Commit

---
+++ Word Effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word Effects
+//
+//  Created by Errol Cheong on 2017-06-26.
+//  Copyright © 2017 Errol Cheong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
